3f1e6c690fff35e50cf3b494b184799b11551e99
Ренькас Альберт Олегович <ipz235_rao@student.ztu.edu.ua>
2023-12-21 17:17:19 +0200

    додано максимальне із чисел, що зустрічається в заданій матриці більше одного разу

--- Sana04/Program.cs
+++ Sana04/Program.cs
@@ -12,7 +12,7 @@ internal class Program
         int[,] arr = new int[n, m];
         Random rand = new Random();
         int kilkistdodatnixelementiv = 0;
-       
+
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
@@ -21,7 +21,6 @@ internal class Program
             }
         }
 
-      
         Console.WriteLine("Отримана матриця:");
 
         for (int i = 0; i < n; i++)
@@ -30,11 +29,12 @@ internal class Program
             {
                 Console.Write(arr[i, j] + " ");
             }
-            Console.WriteLine(); 
+            Console.WriteLine();
         }
-        for(int i = 0;i < n; i++)
+
+        for (int i = 0; i < n; i++)
         {
-               for(int j = 0;j < m; j++)
+            for (int j = 0; j < m; j++)
             {
                 if (arr[i, j] >= 0)
                 {
@@ -42,8 +42,50 @@ internal class Program
                 }
             }
         }
-        Console.WriteLine("Кількість додатніх елементів в масиві = "+ kilkistdodatnixelementiv);
+
+        Console.WriteLine("Кількість додатніх елементів в масиві = " + kilkistdodatnixelementiv);
+
+     
+        int maxvalue = FindMaxValue(arr);
+        int kilkistmaxvalue = CountMaxValue(arr, maxvalue);
+
+        Console.WriteLine("Максимальне число: " + maxvalue);
+        Console.WriteLine("Кількість максимального числа: " + kilkistmaxvalue);
+    }
+
+    static int FindMaxValue(int[,] arr)
+    {
+        int maxvalue = int.MinValue;
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                if (arr[i, j] > maxvalue)
+                {
+                    maxvalue = arr[i, j];
+                }
+            }
+        }
+
+        return maxvalue;
+    }
+
+    static int CountMaxValue(int[,] arr, int maxvalue)
+    {
+        int count = 0;
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                if (arr[i, j] == maxvalue)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
-
-
